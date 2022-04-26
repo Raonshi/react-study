b0b61c452d134285131db373c8217b40e1f0826c
홍순원 <hongsun-won@hongsun-won-ui-MacBookAir.local>
2022-04-27 01:02:32 +0900

    separate components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import Subject from './components/subject/Subject';
+import TOC from './components/toc/TOC';
+import Content from './components/content/Content';
 
 class App extends Component {
   render() {
@@ -8,47 +10,10 @@ class App extends Component {
       <div className="App">
         <Subject title='HiHi' subtitle='Oh, Hi!'></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title='Content' text='This is Content text'></Content>
       </div>
     );
   }
 }
-
-class Subject extends Component{
-  render(){
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.subtitle}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component{
-  render(){
-    return (
-      <nav>
-          <ul>
-              <li><a href="1.html">HTML</a></li>
-              <li><a href="2.html">CSS</a></li>
-              <li><a href="3.html">JavaScript</a></li>
-          </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return (
-      <article>
-          <h2>HTML</h2>
-          HTML is HyperText Markup Language.
-      </article>
-    );
-  }
-}
-
 
 export default App;
